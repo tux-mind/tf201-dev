e151245f8f72918f682f9996b8f198653389db66
tux-mind <massimo.dragano@gmail.com>
2013-01-22 17:04:48 +0100

    moved zImage to kernel directory. moved initramfs contents to initramfs directory. fixed scripts paths. added CONFIG_SYSVIPC to JB15.patch. updated zImage with CONFIG_SYSVIPC. update test.blob/test.LNX with last kernel.

--- scripts/reflash_kernel.sh
+++ scripts/reflash_kernel.sh
@@ -1,2 +1,2 @@
-./test/abootimg/abootimg --create ./test/test.LNX -k ./test/zImage -r ./test/myinitrd.gz && ./BlobTools/blobpack test/test.blob LNX test/test.LNX
+abootimg --create ./test/test.LNX -k ./kernel/zImage -r ./test/myinitrd.gz && blobpack test/test.blob LNX test/test.LNX
 fastboot -i 0x0b05 flash boot test/test.blob
