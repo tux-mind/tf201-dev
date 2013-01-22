cb1ba6c9fe1f77af2f8728f3c78b76fde725f35a
tux-mind <massimo.dragano@gmail.com>
2013-01-22 17:04:48 +0100

    moved zImage to kernel directory. moved initramfs contents to initramfs directory. fixed scripts paths. added CONFIG_SYSVIPC to JB15.patch. updated zImage with CONFIG_SYSVIPC. update test.blob/test.LNX with last kernel.

--- scripts/make_initrd.sh
+++ scripts/make_initrd.sh
@@ -1,4 +1,10 @@
-rm ../myinitrd.gz
-find . | cpio --create --format='newc' > ../myinitrd
-gzip ../myinitrd
-chmod 777 ../myinitrd.gz
+if [ ( ! -d initramfs ) -o  ( ! -d test ) ]; then 
+	echo "please, execute scripts from tf201-dev root."; 
+	exit;
+fi
+rm test/myinitrd.gz
+cd initramfs
+find . | cpio --create --format='newc' > ../test/myinitrd
+cd ..
+gzip test/myinitrd
+chmod 777 test/myinitrd.gz
