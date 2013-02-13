29b881746c89c102114701d132a80df6617ff4dc
tux-mind <massimo.dragano@gmail.com>
2013-02-13 18:45:38 +0100

    created version 2. kernel: less asus spam in dmesg kernel: asusdec ioctl support ASUSDEC_TP_TOOGLE kernel: show touchpad status in /sys/devices/platform/tegra-i2c.1/i2c-1/1-0019/ec_tp_enable tp_control: added support for ASUSDEC_TOOGLE boot_chooser: tested booting from ext4 .img files initramfs: updated /init TODO: test root_patch.tar.gz on v1 kernel.

--- touchpad_control/tp_control.c
+++ touchpad_control/tp_control.c
@@ -10,27 +10,14 @@
 #define ASUSDEC_TP_CONTROL 0x8004F405
 #define ASUSDEC_TP_ON 1
 #define ASUSDEC_TP_OFF 0
+#define ASUSDEC_TP_TOOGLE	2
+#define IOCTL_DEVICE "/dev/asusdec"
 
-void usage(char *argv0, int exit_status,FILE *stream)
+int do_ioctl_call(int arg)
 {
-	fprintf(stream,"control asusdec-elantech touchpad\nUsage: %s <on|off>\n",argv0);
-	exit(exit_status);
-}
+	int fd;
 
-int main(int argc,char **argv)
-{
-	int fd,arg;
-
-	if(argc != 2)
-		usage(argv[0],EXIT_SUCCESS,stdout);
-
-	if(!strncmp("on",argv[1],3))
-		arg = ASUSDEC_TP_ON;
-	else if(!strncmp("off",argv[1],4))
-		arg = ASUSDEC_TP_OFF;
-	else
-		usage(argv[0],EXIT_FAILURE,stderr);
-	if((fd = open("/dev/asusdec",O_WRONLY)) == -1)
+	if((fd = open(IOCTL_DEVICE,O_WRONLY)) == -1)
 	{
 		perror("open()");
 		return EXIT_FAILURE;
@@ -43,4 +30,29 @@ int main(int argc,char **argv)
 	}
 	close(fd);
 	return EXIT_SUCCESS;
+}
+
+void usage(char *argv0, int exit_status,FILE *stream)
+{
+	fprintf(stream,"control asusdec-elantech touchpad\nUsage: %s [on|off]\n\nif no argument is given try toogle current state ( require v2 kernel ).\n",argv0);
+	exit(exit_status);
+}
+
+int main(int argc,char **argv)
+{
+	int arg;
+
+	if(argc == 1) // no option given, try to toogle status
+		return do_ioctl_call(ASUSDEC_TP_TOOGLE);
+	else if(argc > 2)
+		usage(argv[0],EXIT_FAILURE,stderr);
+	else if(!strncmp("--help",argv[1],7) || !strncmp("-h",argv[1],3))
+		usage(argv[0],EXIT_SUCCESS,stdout);
+	else if(!strncmp("on",argv[1],3))
+		return do_ioctl_call(ASUSDEC_TP_ON);
+	else if(!strncmp("off",argv[1],4))
+		return do_ioctl_call(ASUSDEC_TP_OFF);
+	else
+		usage(argv[0],EXIT_FAILURE,stderr);
+	return EXIT_FAILURE;
 }
