f7b8d5f5b287df98482f392ce3b6dfd41908321e
tux-mind <massimo.dragano@gmail.com>
2013-02-24 19:09:39 +0100

    i forgot some files

--- scripts/make_initrd.sh
+++ scripts/make_initrd.sh
@@ -2,10 +2,10 @@
 STARTWD=$(pwd)
 script_dir=$(dirname "$0")
 cd "$script_dir"
-rm ../test/myinitrd.gz
+rm ../test/initramfs.gz
 cd ../initramfs
 find . | cpio --create --format='newc' > ../test/initramfs
 cd ..
-gzip test/iniramfs
+gzip test/initramfs
 chmod 777 test/initramfs.gz
 cd "$STARTWD"
--- scripts/reflash_kernel.sh
+++ scripts/reflash_kernel.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+if [ "$1" ]; then
+	blob=$(find "$1" -iname "*.blob" | tail -n1);
+	echo "going to flash $blob"
+	fastboot -i 0x0b05 flash boot "$blob" && \
+	fastboot -i 0x0b05 reboot
+	exit
+fi
 script_dir=$(dirname $0)
 cd "$script_dir"
-abootimg --create ../test/test.LNX -k ../test/zImage -r ../test/initramfs.gz && \
-blobpack ../test/test.blob LNX ../test/test.LNX && \
-fastboot -i 0x0b05 flash boot ../test/test.blob && \
+abootimg --create "../test/test.LNX" -k "../test/zImage" -r "../test/initramfs.gz" && \
+blobpack "../test/test.blob" LNX "../test/test.LNX" && \
+fastboot -i 0x0b05 flash boot "../test/test.blob" && \
 fastboot -i 0x0b05 reboot
 cd -
