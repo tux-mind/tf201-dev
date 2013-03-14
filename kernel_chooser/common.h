f655569cfae003838d20467ad7de1a9da702fe82
tux-mind <massimo.dragano@gmail.com>
2013-03-14 19:18:26 +0100

    done with debugging ( i hope )

--- kernel_chooser/common.h
+++ kernel_chooser/common.h
@@ -1,5 +1,5 @@
 // enable various options such as debug statements and extra pauses.
-#define DEVELOPMENT
+//#define DEVELOPMENT
 
 // print helpers
 #define FATAL(x,args...)	do{printf("[FATAL] "x,##args);printed_lines++;fatal_error=1;}while(0)
@@ -7,10 +7,7 @@
 #define WARN(x,args...)		do{printf("[WARN ] "x,##args);printed_lines++;}while(0)
 #define INFO(x,args...)		do{printf("[INFO ] "x,##args);printed_lines++;}while(0)
 #ifdef DEVELOPMENT
-#define DEBUG(x,args...) 	do{printf("[DEBUG %s ] "x,__func__,##args);printed_lines++;}while(0)
-#undef ERROR
-#define ERROR(x,args...)	do{printf("[ERROR %s ] "x,__func__,##args);printed_lines++;}while(0)
-#define STOP_BEFORE_MENU
+#define DEBUG(x,args...) 	do{printf("[DEBUG] "x,##args);printed_lines++;}while(0)
 #define SHELL // allow the user to drop into a shell provided by busybox
 #else
 #define DEBUG(x,args...)
