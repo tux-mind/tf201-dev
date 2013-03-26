9f9529b072b2dcd5edc2e05f40f8add4e7a035cd
Rowan Decker <Smasher816@gmail.com>
2013-03-25 22:54:01 -0500

    Update scripts
    Final update for the night. With this I no longer have to do edits to the repo to compile. I just have to export a few environement variables related to my setup. This will also stop me from doing partial pushes and all that silly stuff, keeping me with the HEAD.

--- scripts/reflash_kernel.sh
+++ scripts/reflash_kernel.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
+
+#export NVFLASH if you want to use that instead of fastboot
+
+flash() {
+	if [ -n $2 ]; then
+		wheelie --blob ./blob.bin && \
+		nvflash -r --download 6 $1 && \
+		nvflash -r --go && \
+		exit 0
+	else
+		fastboot -i 0x0b05 flash boot $1 && \
+		fastboot -i 0x0b05 reboot && \
+		exit 0
+	fi
+	exit 1
+}
+
+script_dir=$(dirname $0)
+cd "$script_dir"
+
 if [ "$1" ]; then
 	blob=$(find "$1" -iname "*.blob" | tail -n1);
 	echo "going to flash $blob"
-	fastboot -i 0x0b05 flash boot "$blob" && \
-	fastboot -i 0x0b05 reboot
+	flash $blob
 	exit
 fi
-script_dir=$(dirname $0)
-cd "$script_dir"
-abootimg --create "../test/test.LNX" -k "../test/zImage" -r "../test/initramfs.gz" && \
-blobpack "../test/test.blob" LNX "../test/test.LNX" && \
-fastboot -i 0x0b05 flash boot "../test/test.blob" && \
-fastboot -i 0x0b05 reboot
-cd -
+
+abootimg --create "../test/test.LNX" -k "../test/zImage" -r "../test/initramfs.gz"
+
+if [ "$NVFLASH" ]; then
+	flash ../test/test.LNX 1 && \
+	cd -
+else
+
+	blobpack "../test/test.blob" LNX "../test/test.LNX" && \
+	flash ../test/test.blob && \
+	cd -
+fi
