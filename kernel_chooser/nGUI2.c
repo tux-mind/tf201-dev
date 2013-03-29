396077475f0fba6d6031618d95ce8fa12f1da3cb
Rowan Decker <Smasher816@gmail.com>
2013-03-29 12:12:46 -0500

    Forgot to add nGUI2.*

---
+++ kernel_chooser/nGUI2.c
@@ -0,0 +1,400 @@
+#include <curses.h>
+#include <menu.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <wait.h>
+
+#include "common3.h"
+#include "menu3.h"
+#include "nGUI.h"
+
+int	menu_sizex, // size fo the menu_window ( getmaxyx does not work )
+		menu_sizey,
+		entries_count; // count of created entries ( useful for error handling )
+ITEM **items; // ncurses menu items
+MENU *menu; // ncurses menu
+WINDOW 	*menu_window, // ncurses menu window
+				*messages_win; // ncurses messages window
+char 	**local_entries; // our padded copy of the items names
+
+struct _default_entries
+{
+	const char *name;
+	const int num;
+};
+
+const struct _default_entries default_entries[] =
+{
+	{
+		.name = "reboot",
+		.num = MENU_REBOOT
+	},
+	{
+		.name = "shutdown",
+		.num = MENU_HALT
+	},
+	{
+		.name = "recovery",
+		.num = MENU_RECOVERY
+	}
+#ifdef SHELL
+	,
+	{
+		.name = "emergency shell",
+		.num = MENU_SHELL
+	}
+#endif
+};
+
+void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string)
+{
+	int length, x, y;
+	float temp;
+
+	if(win == NULL)
+		win = stdscr;
+	getyx(win, y, x);
+	if(startx != 0)
+		x = startx;
+	if(starty != 0)
+		y = starty;
+	if(width == 0)
+		width = 80;
+
+	length = strlen(string);
+	temp = (width - length)/ 2;
+	x = startx + (int)temp;
+	mvwprintw(win, y, x, "%s", string);
+	refresh();
+}
+
+/** copy src string to *dst, padding equally from right and left
+ * @dest: destination char *
+ * @src: source char *
+ * @sizex: size of the padded string
+*/
+int copy_with_padd(char **dest, int sizex, char *src)
+{
+	int len;
+
+	len = strlen(src);
+	*dest = malloc((sizex+1)*sizeof(char));
+	if(!*dest)
+	{
+		FATAL("malloc - %s\n",strerror(errno));
+		return -1;
+	}
+	memset(*dest,' ',sizex);
+	strncpy((*dest + ((sizex - len)/2)),src,len);
+	*((*dest)+sizex)='\0';
+	return 0;
+}
+
+int nc_init(void)
+{
+	int sizex,sizey;
+
+	/* Initialize variables to free in case of errors */
+	menu = NULL;
+	messages_win = menu_window = NULL;
+	items = NULL;
+	local_entries = NULL;
+	entries_count = 0;
+
+	/* Initialize curses */
+	initscr(); // TODO: error checking
+	start_color();
+	cbreak();
+	noecho();
+	curs_set(0);
+	keypad(stdscr, TRUE);
+
+	// colors
+	init_pair(COLOR_LOG_DEBUG, COLOR_CYAN, COLOR_BLACK);
+	init_pair(COLOR_LOG_WARN, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(COLOR_LOG_ERROR, COLOR_RED, COLOR_BLACK);
+
+	/* Create messages window */
+	sizey = (LINES * MSG_HEIGHT_PERC)/100;
+	sizex = (COLS * MSG_WIDTH_PERC)/100;
+	messages_win = newwin(sizey-2,sizex-2,(LINES-sizey)+1,1);
+	scrollok(messages_win,TRUE);
+
+	attron(COLOR_PAIR(COLOR_LOG_BORDER));
+	mvaddch(LINES-sizey,0,ACS_ULCORNER);
+	mvhline(LINES-sizey,1,ACS_HLINE,sizex-2);
+	mvaddch(LINES-sizey,sizex-1,ACS_URCORNER);
+	mvaddch(LINES-1,0,ACS_LLCORNER);
+	mvhline(LINES-1,1,ACS_HLINE,sizex-2);
+	mvaddch(LINES-1,sizex-1,ACS_LRCORNER);
+	mvvline(LINES-sizey+1,0,ACS_VLINE,sizey-2);
+	mvvline(LINES-sizey+1,sizex-1,ACS_VLINE,sizey-2);
+	wrefresh(messages_win);
+	refresh();
+	attroff(COLOR_PAIR(COLOR_LOG_BORDER));
+	return 0;
+}
+
+void nc_destroy_menu(void)
+{
+	unpost_menu(menu);
+	free_menu(menu);
+	delwin(menu_window);
+	if(items[entries_count]) // items are NULL-terminated
+		free_item(items[entries_count]);
+	while(entries_count--)
+	{
+		free(local_entries[entries_count]);
+		free_item(items[entries_count]);
+	}
+	free(items);
+	free(local_entries);
+}
+
+void nc_destroy(void)
+{
+	/*delwin(messages_win);
+	 * WARNING: call this function here causes a kernel panic.
+	 * NOTE:DEBUG("messages_win=%p",messages_win) return a valid pointer...
+	 * we have to find why this happens
+	 * NOTE: btw, kernel saying that all memory has freed...strange behaviour.
+	 */
+	clear();
+	endwin();
+}
+
+void draw_menu_border(void)
+{
+	int startx,starty,cur_y,cur_x,len;
+	/* Print a border around the main window and print a title */
+	attron(COLOR_PAIR(COLOR_MENU_BORDER));
+	getbegyx(menu_window,starty,startx);
+	len = strlen(PROMPT);
+	cur_y=starty-1;
+	cur_x=startx;
+	mvhline(cur_y, cur_x, ACS_HLINE, menu_sizex);
+	cur_y-=2;
+	mvhline(cur_y, cur_x, ACS_HLINE, menu_sizex);
+	cur_y=starty+menu_sizey;
+	mvhline(cur_y, cur_x, ACS_HLINE, menu_sizex);
+	mvaddch(cur_y,cur_x-1,ACS_LLCORNER);
+	cur_y = starty-2;
+	cur_x = startx-1;
+	mvvline(cur_y,cur_x, ACS_VLINE, menu_sizey+2);
+	mvprintw(cur_y,cur_x + ((menu_sizex - len)/2),"%s",PROMPT);
+	mvaddch(cur_y-1,cur_x,ACS_ULCORNER);
+	cur_x = startx+menu_sizex;
+	mvvline(cur_y,cur_x, ACS_VLINE, menu_sizey+2);
+	mvaddch(cur_y-1,cur_x,ACS_URCORNER);
+	cur_y=starty+menu_sizey;
+	mvaddch(cur_y,cur_x,ACS_LRCORNER);
+	wrefresh(menu_window);
+	refresh();
+	attroff(COLOR_PAIR(COLOR_MENU_BORDER));
+}
+
+int nc_compute_menu(menu_entry *list)
+{
+	int n_choices, default_count;
+	menu_entry *current;
+
+	/* Compute menu size */
+	menu_sizey = (LINES * MENU_HEIGHT_PERC)/100;
+	menu_sizex = (COLS * MENU_WIDTH_PERC)/100;
+
+	menu_sizex-=2; // borders
+	menu_sizey-=5; // borders + head
+
+	/* Create items */
+	// count entries
+	for(n_choices=0,current=list;current;current=current->next,n_choices++);
+	// count the default ones
+	default_count = ARRAY_SIZE(default_entries);
+	// sum
+	n_choices+= default_count;
+	items = (ITEM **)malloc((n_choices+1)*sizeof(ITEM *));
+	local_entries = malloc((n_choices+1)*sizeof(char *));
+	if(!items || !local_entries)
+	{
+		FATAL("malloc - %s\n",strerror(errno));
+		goto error;
+	}
+	// create default entries
+	for(entries_count=0;entries_count<default_count;entries_count++)
+		if(copy_with_padd(local_entries+entries_count,menu_sizex,(char*)default_entries[entries_count].name))
+			goto error;
+		else
+		{
+			// HACK: store src pointer in item description
+			items[entries_count] = new_item(local_entries[entries_count], default_entries[entries_count].name);
+			if(!items[entries_count])
+			{
+				free(local_entries[entries_count]);
+				FATAL("new_item - %s\n",strerror(errno));
+				goto error;
+			}
+		}
+	for(current=list;current;current=current->next,entries_count++)
+		if(copy_with_padd(local_entries+entries_count,menu_sizex,current->name))
+			goto error;
+		else
+		{
+			// HACK: store src pointer in item description
+			items[entries_count] = new_item(local_entries[entries_count], current->name);
+			if(!items[entries_count])
+			{
+				free(local_entries[entries_count]);
+				FATAL("new_item - %s\n",strerror(errno));
+				goto error;
+			}
+		}
+	items[entries_count] = new_item(NULL,NULL);
+	/* Create menu */
+	menu = new_menu((ITEM **)items);
+	if(!menu)
+	{
+		FATAL("new_menu - %s\n",strerror(errno));
+		goto error;
+	}
+	/* Set menu option not to show the description */
+	menu_opts_off(menu, O_SHOWDESC);
+
+	/* Create the window to be associated with the menu */
+	menu_window = newwin( menu_sizey, menu_sizex, (LINES-menu_sizey)/2, (COLS-menu_sizex)/2);
+	if(!menu_window)
+	{
+		FATAL("newwin - %s\n",strerror(errno));
+		goto error;
+	}
+
+	keypad(menu_window, TRUE);
+	if(wattron(menu_window,COLOR_PAIR(COLOR_MENU_BORDER))==ERR)
+		ERROR("wattron - %s\n",strerror(errno));
+	/* Set main window and sub window */
+	if(set_menu_win(menu, menu_window)==ERR)
+	{
+		FATAL("set_menu_win - %s\n",strerror(errno));
+		goto error;
+	}
+	// set menu size
+	if(set_menu_format(menu, menu_sizey,1) != E_OK)
+		ERROR("set_menu_format - %s\n",strerror(errno));
+
+	/* Set menu mark  */
+	if(set_menu_mark(menu, NULL)!= E_OK)
+		ERROR("set_menu_mark - %s\n",strerror(errno));
+
+	draw_menu_border();
+	return 0;
+	error:
+	nc_destroy_menu();
+	return -1;
+}
+
+int nc_get_user_choice(menu_entry *list)
+{
+	int c,default_count;
+	default_count = ARRAY_SIZE(default_entries);
+
+	/* Post the menu */
+	if(post_menu(menu)!=E_OK)
+		goto error;
+	wrefresh(menu_window);
+	wrefresh(messages_win);
+	refresh();
+
+	while((c = wgetch(menu_window)) != 10)
+	{
+		DEBUG("key %i (%c)\n", c, c);
+		switch(c)
+		{
+			case 278:
+			case KEY_DOWN:
+				menu_driver(menu, REQ_DOWN_ITEM);
+				break;
+			//case KEY_VOLUMEUP: TODO
+			case KEY_UP:
+				menu_driver(menu, REQ_UP_ITEM);
+				break;
+			case KEY_NPAGE:
+				menu_driver(menu, REQ_SCR_DPAGE);
+				break;
+			case KEY_PPAGE:
+				menu_driver(menu, REQ_SCR_UPAGE);
+				break;
+		}
+		wrefresh(menu_window);
+	}
+
+	// HACK: use c as temporary variable
+	c = item_index(current_item(menu));
+	if (c < default_count)
+		c= default_entries[c].num;
+	else if (c == default_count)
+		c = -5;
+	else
+		c = c - default_count + 1;
+	return c;
+
+	error:
+	return MENU_FATAL_ERROR;
+}
+
+void nc_push_message(int i, char *prefix, char *fmt,...)
+{
+	va_list ap;
+
+	if(!messages_win)
+		return;
+
+	va_start(ap,fmt);
+	wattron(messages_win, COLOR_PAIR(i));
+	wprintw(messages_win,prefix);
+	wattroff(messages_win, COLOR_PAIR(i));
+	vwprintw(messages_win,fmt,ap);
+	wrefresh(messages_win);
+	va_end(ap);
+}
+
+void nc_wait_enter(void)
+{
+	while(getch() != 10);
+}
+
+void nc_print_header(void)
+{
+	const char *strings[] = HEADER;
+	int y,x;
+
+	for(y=0;strings[y];y++)
+	{
+		x = (COLS-strlen(strings[y]))/2;
+		mvprintw(y,x,"%s",strings[y]);
+	}
+}
+
+/** wait for a keypress while coutdown.
+ * if user press something return 0.
+ * -1 otherwise
+ */
+int nc_wait_for_keypress(void)
+{
+	int x,y,timeout;
+
+	y = (LINES/2)-1;
+	x = (COLS - snprintf(NULL,0,WAIT_MESSAGE,0))/2;
+
+	timeout(1000); // wait one second between keypress checks
+
+	for (timeout=TIMEOUT_BOOT; timeout>0; timeout--) {
+		mvprintw(y,x,WAIT_MESSAGE, timeout);
+		refresh();
+		if (getch() != ERR) {
+			mvprintw(y,x,"%*s",COLS-x-1," ");
+			return 0;
+		}
+	}
+	return -1;
+}
---
+++ kernel_chooser/nGUI2.h
@@ -0,0 +1,42 @@
+/* our functions return integers
+ * i known that a char it's an int value,
+ * but user can have an entry ID which have the same value of a char.
+ * so i decided to use negative numbers for special entries.
+ */
+#define MENU_REBOOT		-1
+#define MENU_HALT 		-2
+#define MENU_RECOVERY	-3
+#define MENU_SHELL		-4
+#define MENU_DEFAULT	-5
+#define MENU_FATAL_ERROR	-6
+
+// percentage of screen used by the menu
+#define MENU_WIDTH_PERC 50
+#define MENU_HEIGHT_PERC 50
+// percentage of screen used by the messages
+#define MSG_HEIGHT_PERC 25
+#define MSG_WIDTH_PERC 100
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+
+#define WAIT_MESSAGE "Automatic boot in %2d..."
+#define TIMEOUT_BOOT 10 /* time to wait for the user to press a key */
+
+#define HEADER 	\
+{\
+	"                      kernel_chooser - version 2                           ",\
+	"                  Open Source rocks! - tux_mind <massimo.dragano@gmail.com>",\
+	"                                     - smasher816 <smasher816@gmail.com>   ",\
+	NULL\
+}
+
+#define PROMPT "choose an option"
+
+
+int nc_compute_menu(menu_entry *list);
+int nc_init(void);
+void nc_destroy(void);
+void nc_destroy_menu(void);
+void nc_wait_enter(void);
+int nc_get_user_choice(menu_entry *list);
+void nc_print_header(void);
+int nc_wait_for_keypress(void);
